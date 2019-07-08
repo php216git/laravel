a86a026e9941595add1e4dfbed7368967f2b1cdd
zhaowenxu <1648305715@qq.com>
2019-07-08 12:57:19 +0800

    add db

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ Route::get('/', function () {
 
 // 模板继承的练习  搭建后台
 Route::resource('/admin','Admin\AdminController');
+
+// 后台无限分类模块
+Route::resource("/admincates","Admin\CatesController");
+
+//前台首页
+Route::resource("/homeindex","Home\IndexController");
